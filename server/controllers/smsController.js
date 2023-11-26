8b4cf2f7eee8163f7dd12f1486152bf5d241697f
Cybre3 <Cybre3@gmail.com>
2023-11-26 00:42:17 -0600

    server: update receive sms and edit returnInProgress

--- server/controllers/smsController.js
+++ server/controllers/smsController.js
@@ -3,6 +3,8 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
+const { Car } = require('../models/carModel');
+
 module.exports = {
     post: {
         sendSMS: (req, res) => {
@@ -30,38 +32,24 @@ module.exports = {
             */
         },
 
-        receiveSMS: (req, res) => {
+        receiveSMS: async (req, res) => {
             const twiml = new MessagingResponse();
             const readyReg = new RegExp('ready', 'i');
+            const { From, Body } = req.body;
+            const phone = From.replace("+1", "");
 
-            if (readyReg.test(req.body.Body)) {
-                twiml.message('Your car is on the way!');
-            }
-            else {
-                twiml.message(
-                    'Are you ready for your car to be returned?'
-                );
-            }
-            console.log(req.body)
-            // client.messages
-            //     .create({
-            //         from: '18446211510',
-            //         body: twiml.toString(),
-            //         to: '16098151154'
-            //     })
-            //     .then(message => {
-            //         res
-            //             .status(200)
-            //             .type('text/xml')
-            //             .send({ 
-            //                 smsStatusCode: message.status, messageInfo: message.sid, 
-            //                 msg: message.body, 
-            //                 info: message 
-            //             })
-            //     })
-            //     .catch(err => {
-            //         res.status(400).send({ smsErrorCode: err.code, smsErrorMessage: err.message })
-            //     });
+            if (!readyReg.test(Body))
+                return twiml.message('Are you ready for your car to be returned?');
+
+            let carIsInSystem = await Car.findOne({ phone });
+            if (!carIsInSystem) return [
+                twiml.message('Your car has already been returned or you have not stored your car.'),
+                res.status(404).send('Car not found.')
+            ];
+
+            carIsInSystem.returnInProgress = true;
+            twiml.message('Your car is on the way!');
+
             res.status(200).type('text/xml').send(twiml.toString());
         }
     }
